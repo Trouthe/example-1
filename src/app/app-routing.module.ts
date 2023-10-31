3b4f8da8468bdb796595932544f2a7b702c471b2
Trouthe <anthony.hajjar00@gmail.com>
2023-10-31 14:24:04 +0200

    fin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
